a32df80a9eef0aae48123d06f379210e94c74329
MasayaYamada <masaya.yamada.stripes@gmail.com>
2020-05-10 02:19:45 +0900

    add animation to swipe delete.

--- lib/home_route.dart
+++ lib/home_route.dart
@@ -51,43 +51,45 @@ class _Home extends State<Home> {
           padding: const EdgeInsets.all(2.0),
           itemBuilder: (BuildContext context, int position) {
             return Card(
-              child: ListTile(
-                title: Text(
-                  '${article[position].title}',
-                  style: TextStyle(
-                      fontSize: 18.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
-                leading: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    child: article[position].urlToImage == null
-                        ? Image(
-                      image: AssetImage('assets/image/noImage.png'),
-                    )
-                        : Image.network('${article[position].urlToImage}'),
-                    height: 100.0,
-                    width: 100.0,
-                  ),
-                ),
-                trailing: _favoriteIconButton(context, position, article[position].title, article[position].url, article[position].urlToImage),
-                onTap: () {
-                  print(article[position].url);
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => WebViewScreen(url: article[position].url)),
-                  );
-                },
-              ),
+              child: listTile(article, position, context),
             );
           }),
     );
   }
 
+  ListTile listTile(List<NewsDataList> article, int position, BuildContext context) {
+    return ListTile(
+              title: Text(
+                '${article[position].title}',
+                style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+              leading: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  child: article[position].urlToImage == null
+                      ? Image(
+                    image: AssetImage('assets/image/noImage.png'),
+                  )
+                      : Image.network('${article[position].urlToImage}'),
+                  height: 100.0,
+                  width: 100.0,
+                ),
+              ),
+              trailing: _favoriteIconButton(context, position, article[position].title, article[position].url, article[position].urlToImage),
+              onTap: () {
+                print(article[position].url);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => WebViewScreen(url: article[position].url)),
+                );
+              },
+            );
+  }
+
   IconButton _favoriteIconButton( BuildContext context, int position, String title, String url, String urlToImage, {IconData iconData}){
-//    print('_isFavorite on favoButton function is $_isFavorite');
-//    print('position is $position');
     return(
         IconButton(
           icon: Icon(
